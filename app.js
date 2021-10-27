31e4bb2fcf3352265947168991c86176ce772229
pshruthi04 <89540632+pshruthi04@users.noreply.github.com>
2021-10-27 18:49:44 -0500

    modidis 3 files

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var myDataRouter = require('./routes/mydata')
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', myDataRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,7 +34,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+ 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
